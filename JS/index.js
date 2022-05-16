d2b2276c3cd4acd8ce8b9df67e9c2d3a8034db07
Gautam <61959145+gautam7-github@users.noreply.github.com>
2022-05-16 19:38:01 +0530

    added force network switch

--- JS/index.js
+++ JS/index.js
@@ -11,17 +11,22 @@ async function connectMetaMask(mtamaskbtn) {
 	}
 	if (window.ethereum) {
 		window.WEB3 = new Web3(window.ethereum);
+		const accounts = await window.WEB3.eth.getAccounts();
+		const account = accounts[0];
+		const chainID = await window.WEB3.eth.getChainId();
+		if (chainID != 3) {
+			await ethereum.request({
+				method: "wallet_switchEthereumChain",
+				params: [{ chainId: "0x3" }],
+			});
+		}
+		sessionStorage.setItem("account", account);
+		mtamaskbtn.remove();
+		window.open(
+			"https://gautam7-github.github.io/simplestorageDapp/contract.html",
+			"_self"
+		);
 	}
-	const accounts = await window.WEB3.eth.getAccounts();
-	const account = accounts[0];
-	const chainID = await window.WEB3.eth.getChainId();
-	sessionStorage.setItem("account", account);
-	console.log(account);
-	mtamaskbtn.remove();
-	window.open(
-		"https://gautam7-github.github.io/simplestorageDapp/contract.html",
-		"_self"
-	);
 }
 window.onload = async function () {
 	const mtamaskbtn = document.getElementById("metamaskBtn");
