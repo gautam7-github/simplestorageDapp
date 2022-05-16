d30c3eee5096a3263411529e35bde03e34b74697
Gautam <61959145+gautam7-github@users.noreply.github.com>
2022-05-16 22:04:16 +0530

    test branch

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,36 @@
 async function connectMetaMask(mtamaskbtn) {
-	if (window.ethereum.isConnected()) {
-		const account = await window.ethereum.request({
-			method: "eth_requestAccounts",
-		});
-		sessionStorage.setItem("account", account);
-		window.open(
-			"https://gautam7-github.github.io/simplestorageDapp/contract.htmlhtml",
-			"_self"
-		);
-	}
-	if (window.ethereum) {
+	try {
+		await window.ethereum.request({ method: "eth_requestAccounts" });
 		window.WEB3 = new Web3(window.ethereum);
-		const accounts = await window.WEB3.eth.getAccounts();
-		const account = accounts[0];
-		await window.WEB3.eth.getChainId().then((val) => {
-			if (val != 3){
-
-				await ethereum.request({
-						method: "wallet_switchEthereumChain",
-						params: [{ chainId: "0x3" }],
-				});
-				
-			}
-		});
-		sessionStorage.setItem("account", account);
-		mtamaskbtn.remove();
-		window.open(
-			"https://gautam7-github.github.io/simplestorageDapp/contract.html",
-			"_self"
-		);
+	} catch (error) {
+		console.log("No Metamask");
 	}
+
+	let account;
+	await window.WEB3.eth.getAccounts().then(function (result) {
+		console.log(result);
+		account = result[0];
+	});
+	sessionStorage.setItem("account", account);
+	console.log(account);
+	let chainVal;
+	await window.WEB3.eth.getChainId().then((val) => {
+		chainVal = val;
+	});
+	if (chainVal != 3) {
+		await ethereum.request({
+			method: "wallet_switchEthereumChain",
+			params: [{ chainId: "0x3" }],
+		});
+	}
+	mtamaskbtn.remove();
+	window.open(
+		"../contract.html",
+		// "https://gautam7-github.github.io/simplestorageDapp/contract.html",
+		"_self"
+	);
 }
 window.onload = async function () {
-	if (typeof ethereum == undefined) {
-		alert("Install Metamask");
-	}
 	const mtamaskbtn = document.getElementById("metamaskBtn");
 	mtamaskbtn.addEventListener("click", () => {
 		connectMetaMask(mtamaskbtn);
